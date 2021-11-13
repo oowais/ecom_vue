aa8b1d8ba0afc12a5ca04bbc0e9e6c698874a5a0
Owais <oowais@gmail.com>
2021-11-13 06:29:30 +0100

    hello world from ecom vue

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <strong>eCom</strong>
+        </router-link>
+        <a
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        class="navbar-menu"
+        id="navbar-menu"
+        :class="{ 'is-active': showMobileMenu }"
+      >
+        <div class="navbar-end">
+          <router-link to="/category1" class="navbar-item"
+            >Category 1</router-link
+          >
+          <router-link to="/category2" class="navbar-item"
+            >Category 2</router-link
+          >
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light"
+                >Login</router-link
+              >
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view />
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright &copy; 2021</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    };
+  },
+};
+</script>
+
+
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
